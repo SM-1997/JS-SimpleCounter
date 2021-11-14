42fb6507a239bd80f43d11e802c179f01d5dacfe
SM <stefanomartelli1997@gmail.com>
2021-11-14 21:06:00 +0100

    Update counter.js

--- JS/counter.js
+++ JS/counter.js
@@ -1,22 +1,20 @@
 const sum1 = document.querySelector('[data-sum1]')
 const sub1 = document.querySelector('[data-sub1]')
 const reset = document.querySelector('[data-reset]')
+const c = document.getElementById('counter')
 
 sum1.addEventListener("click", button => { plus1() })
 sub1.addEventListener("click", button => { substract1() })
 reset.addEventListener("click", button => { resetTo0() })
 
 function plus1() {
-    c = document.getElementById('counter')
     c.innerHTML = parseInt(c.innerHTML) + 1
 }
 
 function substract1() {
-    c = document.getElementById('counter')
     c.innerHTML = parseInt(c.innerHTML) - 1
 }
 
 function resetTo0() {
-    c = document.getElementById('counter')
     c.innerHTML = 0
 }
