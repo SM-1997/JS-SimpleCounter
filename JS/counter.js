5cbbec8df578d7d879282eaa21e34a07ee9afe3d
SM <stefanomartelli1997@gmail.com>
2021-11-14 21:10:04 +0100

    -

--- JS/counter.js
+++ JS/counter.js
@@ -3,18 +3,6 @@ const sub1 = document.querySelector('[data-sub1]')
 const reset = document.querySelector('[data-reset]')
 const c = document.getElementById('counter')
 
-sum1.addEventListener("click", button => { plus1() })
-sub1.addEventListener("click", button => { substract1() })
-reset.addEventListener("click", button => { resetTo0() })
-
-function plus1() {
-    c.innerHTML = parseInt(c.innerHTML) + 1
-}
-
-function substract1() {
-    c.innerHTML = parseInt(c.innerHTML) - 1
-}
-
-function resetTo0() {
-    c.innerHTML = 0
-}
+sum1.addEventListener("click", button => {  c.innerHTML = parseInt(c.innerHTML) + 1 })
+sub1.addEventListener("click", button => {  c.innerHTML = parseInt(c.innerHTML) - 1 })
+reset.addEventListener("click", button => { c.innerHTML = 0 })
